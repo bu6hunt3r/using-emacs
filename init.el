bd0b91254414e33e01edad29572566303bc6d974
Dave Eddy <dave@daveeddy.com>
2020-01-16 10:32:19 +0100

    Added master branch

--- init.el
+++ init.el
@@ -1,3 +1,22 @@
 (require 'org)
 (org-babel-load-file
   (expand-file-name "settings.org" "/home/cr0c0/Dokumente/using-emacs/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+q   (quote
+    ("4138944fbed88c047c9973f68908b36b4153646a045648a22083bd622d1e636d" "0809c08440b51a39c77ec5529f89af83ab256a9d48107b088d40098ce322c7d8" "cb96a06ed8f47b07c014e8637bd0fd0e6c555364171504680ac41930cfe5e11e" default)))
+ '(font-size--current-size size)
+ '(font-size--default-size default-size)
+ '(package-selected-packages
+   (quote
+    (nyan-mode ujelly-theme doom-modeline doom-themes nyx-theme git-gutter counsel swiper lorem-ipsum tabbar ace-window org-bullets which-key try use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
