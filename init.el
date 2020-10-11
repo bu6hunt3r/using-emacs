41bab707b98f939667b0aaaeb1a7223d0bee1b87
bu6hunt3r <bu6hunt3r@web.de>
2020-10-11 17:27:23 +0200

    Added Rust/Golang

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (require 'org)
 (org-babel-load-file
-  (expand-file-name "settings.org" "/home/cr0c0/Dokumente/using-emacs/"))
+  (expand-file-name "settings.org" "/home/bu6hunt3r/Dokumente/using-emacs/"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -10,16 +10,17 @@
  '(crux-shell "/bin/bash")
  '(custom-safe-themes q)
  '(elpy-modules
-   '(elpy-module-company elpy-module-flymake elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
+   (quote
+    (elpy-module-company elpy-module-flymake elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
  '(flycheck-checker-error-threshold 1000)
  '(font-size--current-size size)
  '(font-size--default-size default-size)
- '(helm-youtube-key 'AIzaSyDfs3niGPbVTgnd7CdinQhWfYo9haClBwQ)
- '(nand2tetris-tools-dir '/home/cr0c0/nand2tetris/tools)
- '(org-agenda-files
-   '("~/Dokumente/using-emacs/settings.org" "~/org/brain/todo.org"))
+ '(helm-youtube-key (quote AIzaSyDfs3niGPbVTgnd7CdinQhWfYo9haClBwQ))
+ '(nand2tetris-tools-dir (quote /home/cr0c0/nand2tetris/tools))
+ '(org-agenda-files (quote ("~/Dokumente/using-emacs/settings.org")))
  '(package-selected-packages
-   '(go-complete worf web-mode fzf ryo-modal modalka autopair wavefront-obj-mode frog-jump-buffer dumb-jump tickscript-mode company-jedi openwith undo-fu nand2tetris irony-eldoc undo-tree counsel swiper lorem-ipsum tabbar ace-window org-bullets which-key try use-package))
+   (quote
+    (go-mode go-complete worf web-mode fzf ryo-modal modalka autopair wavefront-obj-mode frog-jump-buffer dumb-jump tickscript-mode company-jedi openwith undo-fu nand2tetris irony-eldoc undo-tree counsel swiper lorem-ipsum tabbar ace-window org-bullets which-key try use-package)))
  '(x86-lookup-pdf "/home/cr0c0/Dokumente/intel.pdf"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
